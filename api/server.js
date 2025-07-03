330217b7031705aebb518a0a3d427e8a81cce09b
Tasnuva Islam <tasnuvamarziya60@gmail.com>
2025-07-04 01:03:18 +0600

    server

--- api/server.js
+++ api/server.js
@@ -1,9 +1,12 @@
 import { create, router as _router, defaults } from "json-server";
+
 const server = create();
 const router = _router("db.json");
 const middlewares = defaults();
 
 server.use(middlewares);
-server.use(router);
+
+// Handle the /api prefix from Vercel routing
+server.use("/api", router);
 
 export default server;
